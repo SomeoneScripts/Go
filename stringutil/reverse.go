ecadb2305dfe67e9c48928a827ed45fcc4656345
Astral Dev 2 <lucasviniciussoaresdasilva100@gmail.com>
2026-06-24 22:50:12 -0300

    Add files via upload

--- stringutil/reverse.go
+++ stringutil/reverse.go
@@ -2,8 +2,8 @@ package stringutil
 
 func reverse(s string) string {
  t := []rune(s)
- for i, e := 0, len(t)-1; i < len(t)/2; i, e := i+1, e-1 {
+ for i, e := 0, len(t)-1; i < len(t)/2; i, e = i+1, e-1 {
   t[i], t[e] = t[e], t[i]
  }
- return t
+ return string(t)
 }
